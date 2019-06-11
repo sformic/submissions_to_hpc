48c88f5e89f842d689b9963dc07f59956909b561
Sara Formichetti <sara.formichetti92@gmail.com>
2019-06-11 13:19:28 +0200

    added if statement for sample being control or treatment

--- hiddenDomains_sbatch.sh
+++ hiddenDomains_sbatch.sh
@@ -12,9 +12,17 @@ chr_sizes_file=$2
 custom_bin_size=$3
 
 for treatment_file in $bam_input_dir/*picardreorder_validprimary_majchr_picardreorder_dupremoved_*
-do
-	sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
-	SRR_id=$(basename $treatment_file | sed 's/.*_//' | sed ' s/\..*$//')
-	input_file=$(ls $bam_input_dir | awk '/Liu_prepupae/ && /picardreorder_validprimary_majchr_picardreorder_dupremoved_/' | egrep 'control|input') 
-	srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id
-done
+	do
+		if [[ ! "$(basename $treatment_file)" =~ control && ! "$(basename $treatment_file)" =~ input ]]; then
+			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
+			SRR_id=$(basename $treatment_file | sed 's/.*_SRR/SRR/' | sed ' s/\..*$//')
+			input_file=$(ls $bam_input_dir | awk '/picardreorder_validprimary_majchr_picardreorder_dupremoved_/' | egrep 'control|input && $sample_type') 
+			srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id
+		else
+			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
+			SRR_id=$(basename $treatment_file | sed 's/.*_SRR/SRR/' | sed ' s/\..*$//')
+			srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -o $sample_type.$SRR_id
+		fi
+	done
+
+			
