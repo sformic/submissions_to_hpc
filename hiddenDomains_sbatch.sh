30ed9b4256907269495163f9c167d56c3f9532aa
Sara Formichetti <sara.formichetti92@gmail.com>
2019-06-12 16:49:31 +0200

    generalized for samples wo respective ctrl

--- hiddenDomains_sbatch.sh
+++ hiddenDomains_sbatch.sh
@@ -15,11 +15,13 @@ for treatment_file in $bam_input_dir/*picardreorder_*
 		if [[ ! "$(basename $treatment_file)" =~ control && ! "$(basename $treatment_file)" =~ input ]]; then
 			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
 			SRR_id=$(basename $treatment_file | sed 's/.*dupremoved_SRR/SRR/' | sed ' s/\..*$//')
-			input_file=$(ls $bam_input_dir | awk '/picardreorder_/' | egrep 'control|input && $sample_type') 
-			srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id.$custom_bin_size
+			ls $bam_input_dir | if egrep -q 'control|input && $sample_type'; then
+				input_file=$(ls $bam_input_dir | awk '/picardreorder_/' | egrep 'control|input && $sample_type') 
+				srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id.$custom_bin_size
+			else
+				srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -o $sample_type.$SRR_id.$custom_bin_size
+			fi	
 		else
-			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
-			SRR_id=$(basename $treatment_file | sed 's/.*dupremoved_SRR/SRR/' | sed ' s/\..*$//')
 			srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -o $sample_type.$SRR_id.$custom_bin_size
 
 		fi
