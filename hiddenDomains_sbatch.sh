e986a53f46c2904ec85b1421eb3b4b841731f639
Sara Formichetti <sara.formichetti92@gmail.com>
2019-06-12 14:45:03 +0200

    correction for generalization

--- hiddenDomains_sbatch.sh
+++ hiddenDomains_sbatch.sh
@@ -10,12 +10,12 @@ bam_input_dir=$1
 chr_sizes_file=$2
 custom_bin_size=$3
 
-for treatment_file in $bam_input_dir/*picardreorder_validprimary_majchr_picardreorder_dupremoved_*
+for treatment_file in $bam_input_dir/*picardreorder_*
 	do
 		if [[ ! "$(basename $treatment_file)" =~ control && ! "$(basename $treatment_file)" =~ input ]]; then
 			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
 			SRR_id=$(basename $treatment_file | sed 's/.*dupremoved_SRR/SRR/' | sed ' s/\..*$//')
-			input_file=$(ls $bam_input_dir | awk '/picardreorder_validprimary_majchr_picardreorder_dupremoved_/' | egrep 'control|input && $sample_type') 
+			input_file=$(ls $bam_input_dir | awk '/picardreorder_/' | egrep 'control|input && $sample_type') 
 			srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id.$custom_bin_size
 		else
 			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
