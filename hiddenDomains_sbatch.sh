f4f95f774572fff9b2879893d691137ce2910d10
Sara Formichetti <sara.formichetti92@gmail.com>
2019-06-13 16:11:35 +0200

    corrected position of some variables' def inside loop

--- hiddenDomains_sbatch.sh
+++ hiddenDomains_sbatch.sh
@@ -12,10 +12,10 @@ custom_bin_size=$3
 
 for treatment_file in $bam_input_dir/*picardreorder_*
 	do
+		sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
+		SRR_id=$(basename $treatment_file | sed 's/.*dupremoved_SRR/SRR/' | sed ' s/\..*$//')
 		if [[ ! "$(basename $treatment_file)" =~ control && ! "$(basename $treatment_file)" =~ input ]]; then
-			sample_type=$(basename $treatment_file | sed 's/_trimgalore_bowtie2_.*$//')
-			SRR_id=$(basename $treatment_file | sed 's/.*dupremoved_SRR/SRR/' | sed ' s/\..*$//')
-			ls $bam_input_dir | if egrep -q 'control|input && $sample_type'; then
+				ls $bam_input_dir | if egrep -q 'control|input && $sample_type'; then
 				input_file=$(ls $bam_input_dir | awk '/picardreorder_/' | egrep 'control|input && $sample_type') 
 				srun /g/funcgen/bin/hiddenDomains -g $chr_sizes_file -b $custom_bin_size -t $treatment_file -c $bam_input_dir/$input_file -o $sample_type.$SRR_id.$custom_bin_size
 			else
